ad6f74edd02f2f5732a7ccbd22aa97e9d6995509
Natasha S Love <debora@inbox.ru>
2018-10-14 23:03:07 -0500

    Initial commit

---
+++ JavaLessonSix.java
@@ -0,0 +1,12 @@
+import java.util.Scanner; // Library - allows to capture user input
+import java.util.*; // Allows to check for InputMismatchException
+import java.io.*; // Allows for system input and output through data streams, serialization and the file system
+
+public classs JavaLessonSix {
+	static Scanner userInput = new Scanner(System.in);
+	
+	public static void main (String[] args){
+		
+		
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# CIS2323_Week08_Homework1
