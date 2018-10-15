116fa7f97f4bdf742704cbf1ac26a5843cc3e491
Natasha S Love <debora@inbox.ru>
2018-10-14 23:06:26 -0500

    funny spelling mistake

--- JavaLessonSix.java
+++ JavaLessonSix.java
@@ -2,7 +2,7 @@ import java.util.Scanner; // Library - allows to capture user input
 import java.util.*; // Allows to check for InputMismatchException
 import java.io.*; // Allows for system input and output through data streams, serialization and the file system
 
-public classs JavaLessonSix {
+public class JavaLessonSix {
 	static Scanner userInput = new Scanner(System.in);
 	
 	public static void main (String[] args){
